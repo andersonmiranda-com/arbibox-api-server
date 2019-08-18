d652752929cb16400d6ec96ff60e5dbf7bb515e8
Anderson Miranda <me@andersonmiranda.com>
2019-08-18 08:57:16 +0200

    orders query mongo

--- data/mongo-orders-query.js
+++ data/mongo-orders-query.js
@@ -1,1 +1,3 @@
-db.getCollection('orders').find({},{_id:0,created_at:1, buy_at:1, sell_at:1,profit1:1}).sort({created_at: -1})
+db.getCollection("orders")
+    .find({}, { _id: 0, created_at: 1, buy_at: 1, sell_at: 1, symbol: 1, profit1: 1 })
+    .sort({ created_at: -1 });
