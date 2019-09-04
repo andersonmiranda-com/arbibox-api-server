d7738c27c99117db01369e2e4273b7c0b42039be
Anderson Miranda <me@andersonmiranda.com>
2019-09-04 14:34:43 +0200

    new withdrawal fees

---
+++ updateFees.js
@@ -0,0 +1,52 @@
+const ccxt = require("ccxt");
+const lodash = require("lodash");
+const colors = require("colors");
+
+const configs = require("./config/settings-arbitrage");
+const db = require("./core/db");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Prepare Tickets
+///
+
+const initialize = async function() {
+    //initialize oppotunities table
+    let exchanges = [];
+    let markets = [];
+
+    if (configs.marketFilter.exchanges) {
+        exchanges = configs.exchanges;
+    } else {
+        exchanges = ccxt.exchanges;
+    }
+
+    if (configs.marketFilter.exchangesBlacklist) {
+        exchanges = lodash.difference(exchanges, configs.exchangesBlacklist);
+    }
+
+    for (let i = 0; i < exchanges.length; i++) {
+        let name = exchanges[i];
+        var start1 = new Date();
+        try {
+            let withdraw = await require("./wihdrawal_fees/" + name);
+
+            let with1 = { ...withdraw };
+
+            Object.keys(withdraw).forEach(function(key) {
+                with1[key] = Number(withdraw[key], 10);
+            });
+
+            let fees = {
+                exchange: name,
+                withdraw: with1
+            };
+            await db.upsertFees(fees);
+        } catch (error) {
+            console.error(colors.red("Error loading Currencies:"), name);
+            console.error(colors.red("Error:"), error.message);
+        }
+    }
+};
+
+initialize();
