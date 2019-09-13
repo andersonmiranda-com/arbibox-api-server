8391f068fcb6a5be3a35fcff81ea9c9e8e07f60a
Anderson Miranda <me@andersonmiranda.com>
2019-09-13 11:58:54 +0200

    data

---
+++ data/mongoexport_ubuntu_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+lastday=$(expr $1 - 1)
+nextday=$(expr $1 + 1)
+
+query1="2019-${2}-${1}T00:00:00Z"
+query2="2019-${2}-${nextday}T00:00:00Z"
+
+fields="opp_created_at,symbol,base,quote,buy_at,sell_at,invest.min.base,invest.min.quote,invest.min.profit,invest.min.profit_percent,invest.max.base,invest.max.quote,invest.max.profit,invest.max.profit_percent"
+
+query="{type:'PA', \$and: [ {opp_created_at :{ \$gt: new Date('${query1}')}}, {opp_created_at :{ \$lt: new Date('${query2}')}}]}"
+
+echo $query
+
+mongoexport -h 192.168.1.35 --db arbibox --collection opportunities --type=csv --fields $fields --out data/opportunities_ubuntu_local-$2-$1.csv --query "${query}"
+
